df4114227c2a35aac51993c1bf45e49c5f6b4d24
iwill <minglq.9@gmail.com>
2016-01-26 17:50:42 +0800

    NBKitDev

---
+++ NBKitDev/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NBKitDev
+//
+//  Created by MingLQ on 2016-01-26.
+//  Copyright © 2016年 iOSNewbies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NBKitDev/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NBKitDev
+//
+//  Created by MingLQ on 2016-01-26.
+//  Copyright © 2016年 iOSNewbies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
