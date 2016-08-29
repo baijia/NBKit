e1a9465c658fc12036725a24423383b609cf2f61
明林清 <minglinqing@baijiahulian.com>
2016-08-29 22:12:30 +0800

    rm podspec.sh

--- podspec.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pod repo update && \
-pod repo push baijiahulian-app-specs NBKit.podspec \
-    --sources=baijiahulian-app-specs,master \
-    --use-libraries \
-    --allow-warnings \
-    --verbose
-
